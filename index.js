488b2e988f41cd7c5271412c23875cf216a8ebf1
Elle Oshiro <ekoshiro16@gmail.com>
2023-03-05 19:57:28 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+Pig Latin is an English language game where you can create an "encrypted" sentence by using a certain translation pattern. In the case of Pig Latin, every word in English is translated by moving the first letter of the English word to the end of the word, followed by the letters "ay". 
+
+For example,
+If you were given a sentence like "hello world",
+The Pig Latin version would read, "ellohay orldway"
+
+If you were given a sentence like "coding is hard",
+the Pig Latin version would read, "odingcay siay ardhay"
+
+Write a function that could translate any string of words into Pig Latin. Have your function output that newly translated string.
+*/
+
+// Write your code below:
